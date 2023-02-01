1465feaaf1cce365061cc0ac78eb691672a51530
baobao <fgg3158@163.com>
2023-02-02 01:08:56 +0800

    feat: 123

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
 import Router from "./router"
+import axios from "axios"
+console.log(2222);
+
+axios.get('http://localhost:9000/rights?_embed=children').then(res=>{
+      console.log(33333,res.data);
+    })
 function App() {
   return (
     <div className="App">
+      888888
       <Router></Router>
     </div>
   );
